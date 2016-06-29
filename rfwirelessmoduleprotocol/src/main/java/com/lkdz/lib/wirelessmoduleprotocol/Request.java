79082465e84e164335c6cb7308e188f8957851c7
chinajl1980 <chinajiliang@hotmail.com>
2016-06-29 14:26:33 +0800

    增加一个更改GPRS表号的命令
    改表号命令在wirelessmoduleprotocol

---
+++ rfwirelessmoduleprotocol/src/main/java/com/lkdz/lib/wirelessmoduleprotocol/Request.java
@@ -0,0 +1,103 @@
+package com.lkdz.lib.wirelessmoduleprotocol;
+
+import android.support.annotation.IntDef;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringDef;
+import android.text.TextUtils;
+import org.apache.commons.*;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.Calendar;
+
+/**
+ * Created by DELL on 2016/6/29.
+ */
+public class Request {
+    @IntDef({EDIT_ID})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Operation {
+    }
+
+    /**
+     * 设置表号
+     */
+    public static final int EDIT_ID = 6;
+
+
+    private void checkId(String id) throws IllegalArgumentException {
+        if (TextUtils.isEmpty(id) || id.length() > 12) {
+            throw new IllegalArgumentException("表号不正确");
+        }
+    }
+
+    private byte[] convertId(String id) {
+        StringBuilder idPaddingLeftWithSpace = new StringBuilder();
+        for (int i = 0; i < 12 - id.length(); i++) {
+            idPaddingLeftWithSpace.append(" ");
+        }
+        idPaddingLeftWithSpace.append(id);
+
+        return idPaddingLeftWithSpace.toString().getBytes();
+    }
+
+    private static byte getSum(byte[] array, int offset, int length) {
+        int sum = 0;
+        for (int i = offset; i < offset + length; i++) {
+            sum += (int) array[i];
+        }
+        return (byte) (sum & 0xFF);
+    }
+
+
+    private Request(Builder builder) {
+    }
+
+    /**
+     * 对终端无线模块的请求指令的构造器。
+     * 使请求指令中参数的设置更加清晰了然。
+     */
+    public static class Builder {
+        public Builder() {
+        }
+
+        /**
+         * 构建请求指令集合
+         * @return Request实例
+         */
+        public Request build() {
+            return new Request(this);
+        }
+    }
+
+    /**
+     *
+     * @param operation 请求指令类型
+     * @param id 无线表号
+     * @return 请求指令的字节数组形式
+     * @throws IllegalArgumentException 无效参数异常
+     */
+    public byte[] create(@Operation int operation, @NonNull String id) throws IllegalArgumentException {
+        checkId(id);
+
+        byte[] array = null;
+        switch (operation) {
+            case EDIT_ID:
+                //region 设置表号
+                array = new byte[19];
+                array[0] = (byte) 0x12;
+                array[1] = (byte) 0x02;
+                array[2] = (byte) 0x99;
+                array[3] = (byte) 0x99;
+                array[4] = (byte) 0x99;
+                array[5] = (byte) 0x99;
+                System.arraycopy(convertId(id), 0, array, 6, 12);
+                array[18] = getSum(array, 1, 23);
+                //endregion
+                break;
+            default:
+                break;
+        }
+        return array;
+    }
+
+}
---
+++ rfwirelessmoduleprotocol/src/main/java/com/lkdz/lib/wirelessmoduleprotocol/Response.java
@@ -0,0 +1,45 @@
+package com.lkdz.lib.wirelessmoduleprotocol;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.StringDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by DELL on 2016/6/29.
+ */
+public class Response {
+    /**
+     * 解析应答数据
+     * @param data 接收到的字节数组
+     * @return MeterValue、MeterTime或Boolean类型的对象
+     * @throws Exception 终端无线模块无任何应答将抛出异常
+     */
+    public static Object resolve(@NonNull byte[] data) throws Exception {
+        if (data == null || data.length == 0) {
+            throw new Exception("接收的数据为Null或Empty");
+        }
+
+        int[] arrayData = new int[data.length];
+        for (int i = 0; i < data.length; i++) {
+            arrayData[i] = data[i] & 0xFF;
+        }
+
+        if (data.length == 16 &&
+                data[1] == 0x01 && data[2] == 0xDD && data[3] == 0x09 &&
+                data[9] == 0x10 && data[14] == 0xF7 && data[15] == 0xC9) {
+            return true;
+        }
+
+        throw new Exception("没有发现符合通讯协议格式的数据");
+    }
+
+    private static int getSum(int[] array, int offset, int length) {
+        int sum = 0;
+        for (int i = offset; i < offset + length; i++) {
+            sum += array[i];
+        }
+        return sum & 0xFF;
+    }
+}
